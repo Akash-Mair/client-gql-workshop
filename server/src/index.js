afea7bef589e060bfd5d6325063b7ba51766c251
Akash Mair <akash.mair@and.digital>
2021-02-01 09:57:06 +0000

    init(server): basic server setup

---
+++ server/src/index.js
@@ -0,0 +1,52 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { data } = require('./musician-data')
+
+
+const typeDefs = gql`
+    type Musician {
+        id: ID!
+        name: String!
+        imageUrl: String
+        grammyWins: Int
+    }
+
+
+    input NewMusician {
+        id: ID!
+        name: String!
+        imageUrl: String
+        grammyWins: Int
+    }
+
+    type Query {
+        musicians: [Musician]!
+        musician(input: ID!): Musician!
+    }
+
+    type Mutation {
+        addMusician(input: NewMusician): Musician!
+    }
+`
+
+const resolvers = {
+    Query: {
+        musicians: (_, __, { musicians }) => musicians,
+        musician: (_, { input }, { musicians }) => musicians.find(x => x.id === input)
+    },
+    Mutation: {
+        addMusician: (_, { input }, { musicians }) => {
+            musicians.push(input)
+            return input
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context() {
+        return { musicians: data }
+    }
+});
+
+server.listen(5000).then(({ url }) => console.log(`running on ${url}`))
